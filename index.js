f34b5b803a91e1d2f4a6b56ae699dcfe06ef7a7b
Brian Arnold <b.arnold09@gmail.com>
2022-01-23 18:08:09 -0500

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(names, events) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+    return countDown;
+}
